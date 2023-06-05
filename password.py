a2fb028d1c568645b1ff8921a7366596d46e8515
MrPluviophile <sisnmi91@gmail.com>
2023-06-06 00:25:52 +0800

    No zero version

--- password.py
+++ password.py
@@ -3,11 +3,13 @@ guesscount = 0
 while guesscount <= 2:
 	guess = input('Please input the password: ')
 	if guess == password:
-		print('Login successful')
 		break
 	else:
 		guesscount = guesscount + 1
+		if guesscount == 3:
+			print('Attempt Limit Exceeded, Fail to Login')
+			break
 		print('Incorrect Password, you have', 3 - guesscount, 'chances.')
-if guesscount == 3:
-	print('Attempt Limit Exceeded, Fail to Login')
+if guesscount != 3:
+	print('Login successful')
 raise SystemExit
